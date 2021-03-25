4e01b419d9bca7adbd40bd91194c0aceb8f493b4
kumarhariomrai <kumarhariomrai@gmail.com>
2021-03-24 21:46:49 -0700

    update sample_config.py

--- config/sample_config.py
+++ config/sample_config.py
@@ -1,17 +1,14 @@
 # You can copy this file to config.py and add your info
 
 # Get API ID and HASH from https://my.telegram.org
-API_ID = 1234567
-API_HASH = "ab1c23def45fg67890h123i45678j9kl"
+API_ID = 3220708
+API_HASH = "38676d5d14b01fbb9fcf4da31f9377d4"
 
 # Get this from @Botfather
-BOT_TOKEN = "1234567890:ABCdEFgHij1KlMNop_QrStuVWxyzuA-EmXI"
+BOT_TOKEN = "1761619322:AAGa77242IO7qTN18epHQUhg7rw3Rpo0nmY"
 
 # The IDs of the users which can stream, skip, pause and change volume
 SUDO_USERS = [
-    383407735,
-    951435494,
-    1392620345
-]
+   1533501801]
 
-CHAT_ID = -1001243271409  # ID of the group where the bot streams
+CHAT_ID = -1001484094986  # ID of the group where the bot streams
